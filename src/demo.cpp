80d3e8b5d2742a218f6dd605ffdca17442114e99
Synarah Sitaf <synarahsitaf@wc122-dhcp-229.brynmawr.edu>
2023-05-10 19:36:36 -0400

    Final Version

--- src/demo.cpp
+++ src/demo.cpp
@@ -5,235 +5,457 @@
 #include <string>
 #include <vector>
 #include "agl/window.h"
-#include "plymesh.h"
-#include "osutils.h"
+#include <functional>
+#include <string>
+#include <random>
+#include <unistd.h>
+
+
 
 using namespace std;
 using namespace glm;
 using namespace agl;
 
-class MeshViewer : public Window {
+
+
+
+class Shooter {
 public:
-    MeshViewer() : Window() {
-    }
+  vec2 position;
+  vec2 size;
+  float speed;
+  vec4 color;
+};
 
-    void setup() {
-        file = GetFilenamesInDir("../models", "ply");
-        for (int i = 0; i < file.size(); i++){
-            PLYMesh curFile;
-            curFile.load("../models/" + file[i]);
-            m.push_back(curFile);
-        }
-        mesh = m[0];
-        //renderer.loadShader("normals", "../shaders/normals.vs", "../shaders/normals.fs");
-        //renderer.loadShader("phong-vertex", "../shaders/phong-vertex.vs", "../shaders/phong-vertex.fs");
-        //renderer.loadShader("phong-pixel", "../shaders/phong-pixel.vs", "../shaders/phong-pixel.fs");
-        //renderer.loadShader("phong-vertex-light", "../shaders/phong-vertex-light.vs", "../shaders/phong-vertex-light.fs");
-        //renderer.loadShader("phong-pixel-light", "../shaders/phong-pixels-light.vs", "../shaders/phong-pixels-light.fs");
-        //renderer.loadShader("toons", "../shaders/toons.vs", "../shaders/toons.fs");
-        //renderer.loadShader("spotlight", "../shaders/spotlight.vs", "../shaders/spotlight.fs");
-        //renderer.loadShader("holes", "../shaders/holes.vs", "../shaders/holes.fs");
-    }
+class Alien {
+public:
+  Alien(vec2 pos, vec2 s, bool alive){
+    position = pos;
+    size = s;
+    isalive = alive;
+  }
+  vec2 position;
+  vec2 size;
+  bool isalive;
+};
 
-    void mouseMotion(int x, int y, int dx, int dy) {
-        if (click) {
-            h = h + dy * 0.1;
-            a = a - dx * 0.1;
-            float x = rad * sin(a) * cos(h);
-            float y = rad * sin(h);
-            float z = rad * cos(a) * cos(h);
-            eyePos = vec3(x, y, z);
-        }
-    }
 
-    void mouseDown(int button, int mods) {
-        click = true;
-    }
+struct Particle {
+  glm::vec3 pos;
+  glm::vec3 vel;
+  glm::vec4 color;
+  float rot;
+  float size;
+  float life;
+  bool dead = false;
+  float radius;
+};
 
-    void mouseUp(int button, int mods) {
-        click = false;
-    }
 
-    void scroll(float dx, float dy) {
-        rad = rad + dy;
-        float x = rad* sin(a) * cos(h);
-        float y = rad*sin(h);
-        float z = rad* cos(a) * cos(h);
-        eyePos = vec3(x, y, z);
-    }
 
-    void keyUp(int key, int mods) {
-      if(key == GLFW_KEY_N){
-         if(curIn == m.size() -1){
-            curIn = 0;
-            std::cout << file[curIn] << std::endl;
-         }
-         else{
-            curIn = curIn + 1;
-            std::cout << file[curIn] << std::endl;
-         }
+class Viewer : public Window {
+public:
+
+
+  Shooter shooter;
+
+  std::vector<Alien> aliens;
+
+  bool gameover = false;
+  bool bulletlost = false;
+  bool restartrequested = false;
+
+  bool lastalienhit = false;
+
+  Viewer() : Window() {
+  }
+
+  void setup() {
+    setWindowSize(1000, 1000);
+    renderer.blendMode(agl::BLEND);
+    renderer.loadShader("texture", "../shaders/texture.vs", "../shaders/texture.fs");
+    renderer.loadTexture("bullet", "../textures/bullet2.png", 0);
+    renderer.loadTexture("shooter", "../textures/shooter2.png", 0);
+    renderer.loadTexture("alien", "../textures/alien.png", 0);
+    renderer.loadTexture("meteor", "../textures/meteor.png", 0);
+    renderer.loadTexture("heart", "../textures/heart2.png", 0);
+    renderer.loadTexture("gameover", "../textures/gameover2.png", 0); 
+    renderer.loadTexture("background", "../textures/background.png",0);
+    renderer.loadTexture("win", "../textures/win.png", 0);
+    meteor.pos = (agl::randomUnitCube() - 0.5f);
+    meteor.pos.y = 0.6;
+    meteor.pos.z = 0;
+    meteor.vel = vec3(0, -0.01, 0);
+    meteor.radius = 0.09;
+    shooter.color = vec4(1.0, 1.0, 0.8, 1.0);
+    bullet.radius = 0.09f;
+    shooter.position = vec2(0, -0.9);
+    shooter.size = vec2(0.3, 0.05);
+    shooter.speed = 0.5f;
+    shooter.color = vec4(0.0f, 1.0f, 1.0f, 1.0f);
+    const float alienwidth = 0.1f;
+    const float alienheight = 0.05f;
+    const float aliengap = 0.01f;
+    const float aliensstartx = -0.45f;
+    const float aliensstarty = 0.5f;
+    for (int i = 0; i < numaliens; i++) {
+      aliens.push_back(Alien(
+        vec2(
+          aliensstartx + (alienwidth + aliengap) * (i % 10),
+          aliensstarty - (alienheight + aliengap) * (i / 10)
+        ),
+        vec2(alienwidth, alienheight),
+        true
+      ));
+    }
+  }
+
+  void updateBullet(){
+    if(!bullet.dead && shot){
+      bullet.pos.y += 0.01;
+    }
+    else if (!bullet.dead && !shot){
+      bullet.pos.x = shooter.position.x;
+      bullet.pos.y = shooter.position.y - 0.1;
+    }
+    else {
+      shot = false;
+      bullet.dead = false;
+      bullet.pos.x = shooter.position.x;
+      bullet.pos.y = shooter.position.y - 0.1;
+    }
+  }
+
+  void restartGame() {
+    alienVel.y = 0;
+    level = 1;
+    hitcount = 0;
+    aliens.clear();
+    numaliens = 10;
+    const float alienwidth = 0.1f;
+    const float alienheight = 0.05f;
+    const float aliengap = 0.01f;
+    const float aliensstartx = -0.45f;
+    const float aliensstarty = 0.5f;
+    for (int i = 0; i < numaliens; i++) {
+      aliens.push_back(Alien(
+        vec2(
+        aliensstartx + (alienwidth + aliengap) * (i % 10),
+        aliensstarty - (alienheight + aliengap) * (i / 10)
+      ),
+      vec2(alienwidth, alienheight),
+      true
+      ));
+  }
+  score = 0;
+  gameover = true;
+}
+
+void nextLevel() {
+    aliens.clear();
+    const int numaliens = level * 10;
+    const float alienwidth = 0.1f;
+    const float alienheight = 0.05f;
+    const float aliengap = 0.01f;
+    const float aliensstartx = -0.45f;
+    const float aliensstarty = 0.5f;
+    for (int i = 0; i < numaliens; i++) {
+      float space = alienheight * (i / 10);
+      aliens.push_back(Alien(
+        vec2(
+        (aliensstartx + (alienwidth + aliengap) * (i % 10)),
+        (aliensstarty - (alienheight + aliengap) + space*2
+        )
+      ),
+      vec2(alienwidth, alienheight),
+      true
+      ));
+  }
+}
+
+
+
+  bool checkBulletAlienCollision() {
+    for (int i = 0; i < aliens.size(); i++) {
+      if (aliens[i].isalive &&
+          bullet.pos.y + bullet.radius > aliens[i].position.y - aliens[i].size.y / 2 &&
+          bullet.pos.y - bullet.radius < aliens[i].position.y + aliens[i].size.y / 2 &&
+          bullet.pos.x > aliens[i].position.x - aliens[i].size.x / 2 &&
+          bullet.pos.x < aliens[i].position.x + aliens[i].size.x / 2) {
+            bullet.dead = true;
+            aliens[i].isalive = false;
+            score++;
+         if (i == aliens.size() - 1) {
+    lastalienhit = true;
+  }
+        return true;
       }
-      else if(key == GLFW_KEY_P){
-         if(curIn == 0){
-            curIn = m.size() -1;
-            std::cout << file[curIn] << std::endl;
-         }
-         else{
-            curIn = curIn - 1;
-            std::cout << file[curIn] << std::endl;
-         }
+    }
+    if(bullet.pos.y > 0.9f){
+      bullet.dead = true;
+    }
+    return false;
+  }
+
+
+    bool checkShooterAlienCollision() {
+    for (int i = 0; i < aliens.size(); i++) {
+      if(aliens[i].isalive && aliens[i].position.y <= shooter.position.y){
+        gameover = true;
       }
-      else if(key == GLFW_KEY_S){
-          if(curSh == sh.size() - 1){
-              curSh = 0;
+      if (aliens[i].isalive &&
+          shooter.position.y + shooter.size.y > aliens[i].position.y - aliens[i].size.y / 2 &&
+          shooter.position.y - shooter.size.y < aliens[i].position.y + aliens[i].size.y / 2 &&
+          shooter.position.x > aliens[i].position.x - aliens[i].size.x / 2 &&
+          shooter.position.x < aliens[i].position.x + aliens[i].size.x / 2) {
+            gameover = true;
+        return true;
+      }
+    }
+    return false;
+  }
+
+
+  void keyEvent(int key, double dt) {
+  if (key == GLFW_KEY_LEFT) {
+    shooter.position.x -= shooter.speed * dt;
+  } else if (key == GLFW_KEY_RIGHT) {
+    shooter.position.x += shooter.speed * dt;
+  }
+}
+
+void keyDown(int key, int action){
+  if (key == GLFW_KEY_UP) {
+    shot = true;
+    }
+}
+
+void drawScore(){
+
+}
+
+void updateAliens(float dt){
+  timePassed += dt;
+  if(timePassed > 1){
+    timePassed = 0;
+    if(aliens[0].position.x < -0.5){
+      alienVel.x = 0.01;
+    }
+    else if(aliens[aliens.size() - 1].position.x >= 0.53){
+      alienVel.x = -0.01;
+    }
+    for(int i = 0; i < aliens.size(); i++){
+      aliens[i].position.x += alienVel.x;
+      aliens[i].position.y += alienVel.y;
+    }
+  }
+}
+
+void updateShooterPosition(float dt) {
+  float newXPos = shooter.position.x;
+  float leftBound = -1.0f + shooter.size.x / 2;
+  float rightBound = 1.0f - shooter.size.x / 2;
+  if (glfwGetKey(window(), GLFW_KEY_LEFT) == GLFW_PRESS) {
+    newXPos = shooter.position.x - shooter.speed * dt;
+  } else if (glfwGetKey(window(), GLFW_KEY_RIGHT) == GLFW_PRESS) {
+    newXPos = shooter.position.x + shooter.speed * dt;
+  }
+  if (newXPos >= leftBound && newXPos <= rightBound) {
+    shooter.position.x = newXPos;
+  }
+}
+
+  void drawBullet(){
+    renderer.beginShader("sprite");
+    renderer.texture("image", "bullet");
+    renderer.push();
+    renderer.scale(vec3(0.3, 1, 1));
+    renderer.pop();
+    renderer.sprite(bullet.pos, vec4(1.0), .02);
+  }
+
+  void drawShooter(){
+    renderer.beginShader("sprite");
+    renderer.texture("image", "shooter");
+    renderer.sprite(vec3(shooter.position, 0), vec4(1.0), 0.1);
+    renderer.endShader();
+  }
+
+  void drawAliens(){
+    renderer.beginShader("sprite");
+    renderer.texture("image", "alien");
+    for (int i = 0; i < aliens.size(); i++) {
+      if (aliens[i].isalive) {
+        renderer.sprite(vec3(aliens[i].position, 0), vec4(1.0), 0.1);
+    }
+  }
+  }
+
+  void updateMeteor(){
+    if(meteorShooterCollision() || meteor.pos.y <= -1.0){
+      meteor.pos.x = agl::randomUnitCube().x;
+      meteor.pos.y = 1.0;
+    }
+    else if(meteorBulletCollision()){
+      meteor.pos.x = agl::randomUnitCube().x;
+      meteor.pos.y = 1.0;
+    }
+    else{
+      meteor.pos += meteor.vel;
+    }
+  }
+  //bigger, two hits to kill
+  void drawMeteor(){
+    renderer.beginShader("sprite");
+    renderer.texture("image", "meteor");
+    renderer.sprite(meteor.pos, vec4(1.0), 0.1);
+    renderer.endShader();
+  }
+
+  bool meteorBulletCollision(){
+    bool hit = false;
+      if (meteor.pos.y + meteor.radius > bullet.pos.y - bullet.radius / 2 &&
+          meteor.pos.y - meteor.radius < bullet.pos.y + bullet.radius / 2 &&
+          meteor.pos.x > bullet.pos.x - bullet.radius / 2 &&
+          meteor.pos.x < bullet.pos.x + bullet.radius / 2) {
+            hit = true;
+            bullet.dead = true;
+            if(hitcount >0){
+              hitcount--;
+            }
           }
-          else{
-              curSh = curSh + 1;
-          } 
+    return hit;
+  }
+
+  bool meteorShooterCollision(){
+    bool hit = false;
+      if (meteor.pos.y + meteor.radius > shooter.position.y - shooter.size.y / 2 &&
+          meteor.pos.y - meteor.radius < shooter.position.y + shooter.size.y / 2 &&
+          meteor.pos.x > shooter.position.x - shooter.size.x / 2 &&
+          meteor.pos.x < shooter.position.x + shooter.size.x / 2) {
+            hit = true;
+            hitcount++;
+          }
+    return hit;
+  }
+
+  void drawLives(){
+    renderer.beginShader("sprite");
+    renderer.texture("image", "heart");
+    renderer.sprite(vec3(0.6, 0.97, 0), vec4(1.0), 0.1);
+    if(hitcount < 4){
+      renderer.sprite(vec3(0.69, 0.97, 0), vec4(1.0), 0.1);
+      if(hitcount < 3){
+        renderer.sprite(vec3(0.78, 0.97, 0), vec4(1.0), 0.1);
+        if(hitcount < 2){
+          renderer.sprite(vec3(0.87, 0.97, 0), vec4(1.0), 0.1);
+          if(hitcount == 0){
+            renderer.sprite(vec3(0.96, 0.97, 0), vec4(1.0), 0.1);
+          }
+        }
       }
-   }
-
-    void draw() {
-      /*
-        renderer.beginShader(sh[curSh]);
-        renderer.setUniform("m.ka", vec3(0.1f));
-        renderer.setUniform("m.kd", vec3(1.0f));
-        renderer.setUniform("m.ks", vec3(0.6f));
-        renderer.setUniform("m.shine", 44.4f);
-        renderer.setUniform("l.la", vec3(0.5f,0.0f,0.5f));
-        renderer.setUniform("l.ld", vec3(0.5f,0.0f,0.5f));
-        renderer.setUniform("l.ls", vec3(0.5f,0.0f,0.5f));
-        renderer.setUniform("l2.la", vec3(1.0f,1.0f,0.0f));
-        renderer.setUniform("l2.ld", vec3(1.0f,1.0f,0.0f));
-        renderer.setUniform("l2.ls", vec3(1.0f,1.0f,0.0f));
-        renderer.setUniform("l2.pos", vec4(5.5f,10.5f,-10.5f,1.0f));
-        renderer.setUniform("l3.la", vec3(1.0f));
-        renderer.setUniform("l3.ld", vec3(1.0f));
-        renderer.setUniform("l3.ls", vec3(1.0f));
-        renderer.setUniform("l3.pos", vec4(25.0f,15.0f,-15.0f,1.0f));
-        renderer.setUniform("l3.exp", 15.0f);
-        renderer.setUniform("l3.co", 30.0f);
-        
-
-
-        mesh = m[curIn]; 
-        float aspect = ((float)width()) / height();
-        renderer.perspective(glm::radians(60.0f), aspect, 0.1f, 50.0f);
-        renderer.lookAt(eyePos, lookPos, up);
-
-       
-        float sx = 1;
-        float sy = 1;
-        float sz = 1;
-        GLfloat xr = mesh.maxBounds().x - mesh.minBounds().x;
-        GLfloat yr = mesh.maxBounds().y - mesh.minBounds().y;
-        GLfloat zr = mesh.maxBounds().z - mesh.minBounds().z;
-        if ((xr > yr) || (xr > zr)) {
-            sx = 5.0f / xr;
-            sy = 5.0f / xr;
-            sz = 5.0f / xr;
-        }
-        else if (yr > zr) {
-            sx = 5.0f / yr;
-            sy = 5.0f / yr;
-            sz = 5.0f / yr;
-        }    
-        else if (zr > yr) {
-            sx = 5.0f / zr;
-            sy = 5.0f / zr;
-            sz = 5.0f / zr;
-
-        }
-        renderer.scale(vec3(sx, sy, sz));
-
-        //renderer.scale(vec3(sin(elapsedTime())*10, cos(elapsedTime())*10, -cos(elapsedTime()*10)));
-        renderer.translate(vec3(0, 0, 0));
-        renderer.lookAt(eyePos,lookPos, up);
-
-        renderer.mesh(mesh);
-        //renderer.cube(); // for debugging!
-
-        
-        renderer.endShader();
-        */
-      //mesh = m[curIn]; 
-      float aspect = ((float)width()) / height();
-      renderer.perspective(glm::radians(60.0f), aspect, 0.1f, 50.0f);
-      renderer.lookAt(eyePos, lookPos, up);
-      //renderer.mesh(mesh);   
-             
-      float sx = 1;
-      float sy = 1;
-      float sz = 1;
-      GLfloat xr = mesh.maxBounds().x - mesh.minBounds().x;
-      GLfloat yr = mesh.maxBounds().y - mesh.minBounds().y;
-      GLfloat zr = mesh.maxBounds().z - mesh.minBounds().z;
-      if ((xr > yr) || (xr > zr)) {
-        sx = 5.0f / xr;
-        sy = 5.0f / xr;
-        sz = 5.0f / xr;
-      }
-      else if (yr > zr) {
-        sx = 5.0f / yr;
-        sy = 5.0f / yr;
-        sz = 5.0f / yr;
-      }    
-        else if (zr > yr) {
-          sx = 5.0f / zr;
-          sy = 5.0f / zr;
-          sz = 5.0f / zr;
-        }
-        //renderer.scale(vec3(sx, sy, sz));
-
-        renderer.push();
-        renderer.translate(vec3(0.0,0.0,0.0));
-        renderer.scale(vec3(2*sx, 2*sy, 2*sz));
-        mesh = m[2];
-        renderer.mesh(mesh);
-        renderer.pop();
-
-        renderer.push();
-        renderer.translate(vec3(0.0,0.0,-0.6));
-        renderer.scale(vec3(0.5*sx, 0.5*sy, 0.5*sz));
-        renderer.rotate(kPI/.70,vec3(1,0,0));
-        mesh = m[3];
-        renderer.mesh(mesh);
-        renderer.pop();
-
-        renderer.push();
-        renderer.translate(vec3(5.0,5.0,5.0));
-        renderer.scale(vec3(3*sx, 3*sy, 3*sz));
-        mesh = m[2];
-        renderer.mesh(mesh);
-        renderer.pop();
-
-        renderer.push();
-        renderer.translate(vec3(10.0,10.0,0.0));
-        renderer.scale(vec3(2*sx, 2*sy, 2*sz));
-        mesh = m[2];
-        renderer.mesh(mesh);
-        renderer.pop();
     }
+  }
+
+  void draw() {
+    
+    
+  if(gameover){
+    elapsed += dt();
+    renderer.beginShader("sprite");
+    renderer.texture("image","gameover");
+    renderer.push();
+    renderer.scale(vec3(1.0, 0.5, 1.0));
+    renderer.sprite(vec3(0.0, 0.0, 0.1), vec4(1.0), 1.0);
+    renderer.pop();
+    if(elapsed > 3){
+      elapsed = 0;
+      gameover = false;
+    }
+  }
+
+  if (level == 4){
+    won = true;
+  }
+  if(won){
+    elapsed += dt();
+    renderer.beginShader("sprite");
+    renderer.texture("image","win");
+    renderer.push();
+    renderer.scale(vec3(1.0, 0.5, 1.0));
+    renderer.sprite(vec3(0.0, 0.0, 0.1), vec4(1.0), 1.0);
+    renderer.pop();
+    if(elapsed > 3){
+      elapsed = 0;
+      won = false;
+      restartGame();
+      gameover = false;
+    }
+  }
+
+  updateShooterPosition(dt());
+  drawShooter();
+  drawBullet();
+  drawMeteor();
+  updateMeteor();
+  updateBullet();
+  updateAliens(dt());
+  drawAliens();
+  checkBulletAlienCollision();
+  drawLives();
+  if(hitcount > 4 || checkShooterAlienCollision()){
+    restartGame();
+  }
+  if(checkDone()){
+    level++;
+    numaliens = level *10;
+    nextLevel();
+    alienVel.y += -0.02;
+  }
+}
+
+bool checkDone(){
+      for(int i = 0; i < aliens.size(); i++){
+        if(aliens[i].isalive){
+          allDead = false;
+          break;
+        }else{
+          allDead = true;
+          
+        }
+      }
+      if(allDead){
+      }
+      return allDead;
+}
 
 protected:
-    std::vector<string> file = GetFilenamesInDir("../models", "ply");
-    PLYMesh mesh;
-    std::vector<PLYMesh> m;
-    std::vector<string> sh = {"normals", "phong-vertex", "phong-pixel","phong-vertex-light","phong-pixel-light","toons","spotlight","holes"};
-    vec3 eyePos = vec3(10, 0, 0);
-    vec3 lookPos = vec3(0, 0, 0);
-    vec3 up = vec3(0, 1, 0);
-    bool click = false;
-    float h = 0;
-    float a = 0;
-    float rad = 10;
-    int curIn = 0;
-    int curSh = 0;
+int numaliens = 10;
+  vec3 alienVel = vec3(0.01, 0, 0);
+  int level = 1;
+  int hitcount = 0;
+  bool allDead = false;
+  Particle bullet;
+  vec3 eyePos = vec3(0, 0, 3);
+  vec3 lookPos = vec3(0, 0, 0);
+  vec3 up = vec3(0, 1, 0);
+  vec3 position = vec3(1, 0, 0);
+  int frames = 0;
+  bool shot = false;
+  std::vector<Particle> mParticles;
+  int index = 0;
+  int score = 0;
+  float timePassed = 0;
+  float alienx = 0.01;
+  int lives = 3;
+  Particle meteor;
+  float elapsed = 0;
+  bool won = false;
 };
 
 int main(int argc, char** argv)
 {
-    MeshViewer viewer;
-    viewer.run();
-    return 0;
+  Viewer viewer;
+  viewer.run();
+  return 0;
 }
